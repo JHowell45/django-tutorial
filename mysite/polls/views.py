152ac5a8b3e2155baf45685a75b419a80aa40631
JHowell45 <jhowell45@googlemail.com>
2021-05-24 22:08:47 +0100

    Create views for pools app.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,29 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from typing import List
+
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World. You're at the polls index.")
+    latest_questions_list: List[Question] = Question.objects.order_by("-pub_date")[:5]
+    return render(
+        request,
+        "polls/index.html",
+        {"latest_question_list": latest_questions_list},
+    )
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/details.html", {"question": question})
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
